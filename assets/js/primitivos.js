848fafb6799be52dc7f62182a107d01d5abbc258
Anderson <andersonlosada.11@gmail.com>
2025-10-22 23:03:45 -0500

    Update index.html to comment out alerts.js and ensure primitivos.js is included

---
+++ assets/js/primitivos.js
@@ -0,0 +1,35 @@
+let nombre = 'Obi';
+console.log(nombre);
+
+nombre = 'Anakin';
+console.log(nombre);
+
+nombre = 'Palpatine';
+nombre = 'Yoda';
+
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esStarWars = false;
+console.log(typeof esStarWars);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.001;
+console.log(typeof edad);
+
+let lightSaberColor;
+console.log(typeof lightSaberColor); //??
+
+let soyNull = null;
+console.log(typeof soyNull); // ???
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
