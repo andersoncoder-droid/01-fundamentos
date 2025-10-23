1cb98ad3eb0afe605d621beed8463c40d588a460
Anderson <andersonlosada.11@gmail.com>
2025-10-22 22:52:15 -0500

    Reorganize alert.js by adding commented-out code snippets for future reference

---
+++ assets/js/alerts.js
@@ -0,0 +1,10 @@
+// alert('Hola mundo');
+
+// let nombre = prompt('¿Cómo te llamas?');
+// console.log(nombre);
+// console.log('****' + nombre + '****');
+
+// const selection = confirm('¿Estás seguro de eliminar esto?');
+// console.log(selection);
+
+// console.log(global);
